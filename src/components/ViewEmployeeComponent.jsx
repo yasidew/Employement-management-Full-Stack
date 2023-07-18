0b0630cc2b7cc7d4b1e97f129047711452d03072
yasidew <it21440922@my.sliit.lk>
2023-07-18 07:24:55 +0530

    commit

--- src/components/ViewEmployeeComponent.jsx
+++ src/components/ViewEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class ViewEmployeeComponent extends Component {
 
@@ -6,15 +7,39 @@ class ViewEmployeeComponent extends Component {
         super(props)
 
         this.state ={
-            id: this.props.match.params.id
+            id: this.props.match.params.id,
+            employee : {}
 
         }
     }
+
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then(res => {
+            this.setState({employee: res.data});
+        })
+    }
+
     render() {
         return (
             <div>
 
-                <h2> View Employee </h2>
+                <div className="card col-md-6 offset-md-3">
+                    <h3 className="text-center">View Employee Details</h3>
+                    <div className="card-body">
+                        <div className="row">
+                            <label>Employee First Name: </label>
+                            <div> {this.state.employee.firstName} </div>
+                        </div>
+                        <div className="row">
+                            <label>Employee last Name: </label>
+                            <div> {this.state.employee.lastName} </div>
+                        </div>
+                        <div className="row">
+                            <label>Employee Email id: </label>
+                            <div> {this.state.employee.emailId} </div>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         );
